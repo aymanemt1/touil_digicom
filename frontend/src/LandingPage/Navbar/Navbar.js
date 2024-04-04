c6cf1e5b4666144e269ab7dcc0799b99b4826085
moutoute Aymane <Moutouteayman10@gmail.com>
2024-04-04 14:27:29 +0000

    fr

--- frontend/src/LandingPage/Navbar/Navbar.js
+++ frontend/src/LandingPage/Navbar/Navbar.js
@@ -1,19 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
 import './Navbar.css'
 
 export const Navbar = () => {
+  const {langue,setlangue}=useState("")
+  
+   
+  const handlLangue =(event)=>{
+      event.preventDefault();
+  }
 
   return (
     <header>
     <nav class="FixedNav">
       <ul id="navbar">
-        <li><a href="#" >About</a></li>
+      
+    <div className='logo'>
+    <li><a href="#"><img src='assets/Logo/logo1.png' /></a></li>
+    </div>
+       <div className='links'>
+       <li><a href="#" >About</a></li>
         <li><a href="#" >Service</a></li>
-        <li><a href="#"><img src='assets/logoTouil.png' /></a></li>
         <li><a href="#">Service</a></li>
         <li><a href="#">About</a></li>
+
+       </div>
+       <div className='right'>
+       <form onSubmit={handlLangue} >
+      <select onChange={(e)=>setlangue(e.target.value)} id='lang' value={langue}>
+        <option value="FR" className='option-fr'>
+  <img src='assets/Flags/france.png' width="10px" />
+        </option>
+        <option value="AR" className='option-ar'>
+          Ar
+        </option>
+      </select>
+    </form>
+    <button class="button" role="button">Demander un devis</button>
+       </div>
       </ul>
+
     </nav>
   </header>
   
